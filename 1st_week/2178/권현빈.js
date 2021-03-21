70fa36b0a1e4806a222cf2421b508c4288bbe57b
bini0823 <bini0823@cau.ac.kr>
2021-03-21 17:27:07 +0900

    코드 잘못올려서 수정

--- 1st_week/2178/권현빈.js
+++ 1st_week/2178/권현빈.js
@@ -1,13 +1,11 @@
 const fs = require("fs");
 const stdin = (process.platform === "linux"
   ? fs.readFileSync("/dev/stdin").toString()
-  : `6 7 3
-.......
-...O...
-....O..
-.......
-OO.....
-OO.....
+  : `4 6
+101111
+101010
+101011
+111011
 `
 ).split("\n");
 
@@ -16,80 +14,38 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-const [row, col, N] = input().split(" ");
+const [row, col] = input().split(" ");
 let arr = [];
 
 for (let i = 0; i < row; i++) {
   arr.push(input().split(""));
 }
-
+arr = arr.map((ele) => ele.map(Number));
 function solution() {
-  const dx = [1, -1, 0, 0];
-  const dy = [0, 0, 1, -1];
-
-  // N이 설치 시점인 경우 바로 출력
-  if (N % 2 === 0) {
-    for (let i = 0; i < row; i++) {
-      let temp = "";
-      for (let j = 0; j < col; j++) {
-        temp += "O";
-      }
-      console.log(temp);
-    }
-    return;
-  }
-
-  // 동서남북 방향 폭발
-  const bomb = (x, y) => {
-    for (let k = 0; k < 4; k++) {
-      let nx = x + dx[k];
-      let ny = y + dy[k];
+  const queue = [];
+  let dx = [1, 0, -1, 0];
+  let dy = [0, 1, 0, -1];
+  const isVisited = new Array(parseInt(row))
+    .fill()
+    .map((ele) => new Array(parseInt(col)).fill(false));
+  queue.push([0, 0]);
+  isVisited[0][0] = true;
+  // bfs
+  while (queue.length) {
+    let [x, y] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
       if (nx > -1 && nx < row && ny > -1 && ny < col) {
-        if (arr[nx][ny] !== "O") {
-          arr[nx][ny] = ".";
-        }
-      }
-    }
-  };
-
-  for (let time = 3; time <= N; time = time + 2) {
-    // 폭발 전 폭발 될 수 있는 부분 처리
-    for (let i = 0; i < row; i++) {
-      for (let j = 0; j < col; j++) {
-        if (arr[i][j] === ".") {
-          arr[i][j] = "T";
-        }
-      }
-    }
-    // 폭발
-    for (let i = 0; i < row; i++) {
-      for (let j = 0; j < col; j++) {
-        if (arr[i][j] === "O") {
-          bomb(i, j);
-        }
-      }
-    }
-    // 폭발 후 자신의 폭탄 자리 폭발, 임시 처리했던 부분 폭탄으로 변화
-    for (let i = 0; i < row; i++) {
-      for (let j = 0; j < col; j++) {
-        if (arr[i][j] === "O") {
-          arr[i][j] = ".";
-        }
-        if (arr[i][j] === "T") {
-          arr[i][j] = "O";
+        if (!isVisited[nx][ny] && arr[nx][ny]) {
+          queue.push([nx, ny]);
+          arr[nx][ny] = arr[x][y] + 1;
+          isVisited[nx][ny] = true;
         }
       }
     }
   }
-
-  // 출력
-  for (let i = 0; i < row; i++) {
-    let temp = "";
-    for (let j = 0; j < col; j++) {
-      temp += arr[i][j];
-    }
-    console.log(temp);
-  }
+  console.log(arr[row - 1][col - 1]);
 }
 
 solution();
