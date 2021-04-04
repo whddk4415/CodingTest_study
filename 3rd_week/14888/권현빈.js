0a44f45c7265802b607fa8ff9893dc8211469d56
bini0823 <bini0823@cau.ac.kr>
2021-04-04 21:43:15 +0900

    3주차 권현빈 백준 14888 미해결

---
+++ 3rd_week/14888/권현빈.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const stdin = (process.platform === "linux"
+  ? fs.readFileSync("/dev/stdin").toString()
+  : `3
+2 2 1
+1 0 1 0
+`
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = input();
+const numArr = input().split(" ").map(Number);
+const operator = [];
+input()
+  .split(" ")
+  .forEach((ele, idx) => {
+    for (let i = 0; i < parseInt(ele); i++) {
+      operator.push(idx);
+    }
+  });
+
+function solution() {
+  let max = -1000000001;
+  let min = 1000000001;
+  const isVisited = Array.from({ length: N - 1 }, (ele) => false);
+
+  const dfs = (v, idx, num, len) => {
+    let result = 0;
+    if (len === N - 1) {
+      if (num > max) {
+        max = num;
+      }
+      if (num < min) {
+        min = num;
+      }
+    } else {
+      for (let i = 0; i < N - 1; i++) {
+        if (!isVisited[i]) {
+          if (operator[i] === 0) {
+            result = num + numArr[idx];
+          } else if (operator[i] === 1) {
+            result = num - numArr[idx];
+          } else if (operator[i] === 2) {
+            result = num * numArr[idx];
+          } else if (operator[i] === 3) {
+            result = parseInt(num / numArr[idx]);
+          }
+          isVisited[i] = true;
+          dfs(i, idx + 1, result, len + 1);
+        }
+      }
+    }
+    isVisited[v] = false;
+  };
+  dfs(0, 1, numArr[0], 0);
+  console.log(max);
+  console.log(min);
+}
+
+solution();
